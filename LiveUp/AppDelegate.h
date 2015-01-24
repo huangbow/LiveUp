b11cc3068da30cd539887b50da3ebd9711879ef2
huangbowen <a@a.com>
2015-01-24 00:25:05 -0800

    Initial Commit

---
+++ LiveUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiveUp
+//
+//  Created by Alex on 1/24/15.
+//  Copyright (c) 2015 Bowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LiveUp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  LiveUp
+//
+//  Created by Alex on 1/24/15.
+//  Copyright (c) 2015 Bowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
